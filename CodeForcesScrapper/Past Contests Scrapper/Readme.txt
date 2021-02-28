e9ebfe1ca46a73fb99605d63fbcff0e0da53b07f
Hemank Bajaj <hemankbajaj05@gmail.com>
2021-02-28 18:38:12 +0530

    added past contest scrapper bonus task 1

---
+++ CodeForcesScrapper/Past Contests Scrapper/Readme.txt
@@ -0,0 +1,28 @@
+********************************************************************************************************************************************************************
+First of all make sure that chromedriver.exe is present in the directory of the script file.
+********************************************************************************************************************************************************************
+How to use?
+
+Open your terminal
+Check that the current working directory should be of this folder only
+Fill all the required input fields
+
+Steps:
+1. Fill in the no. of past contest u need to scrap.
+
+Wait for the browser window to close
+
+After this open the directory of the program and there you will find a folder with all the the past contests.
+The nth most recent constest will be named as Most Recent Contsest n.
+
+********************************************************************************************************************************************************************
+
+How the program works?
+
+First we open the past contests page.
+We find that to open the contest we need to click the link with text "Enter >>".
+We find first x links (x = no. of contests that we need)
+After this we clink on each of these links and scrape each and every problem of every contest
+
+
+********************************************************************************************************************************************************************
--- CodeForcesScrapper/Past Contests Scrapper/past_contests.py
+++ CodeForcesScrapper/Past Contests Scrapper/past_contests.py
@@ -6,7 +6,57 @@ number_int = int(number_str)
 
 PATH = "chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-
 link = "https://codeforces.com/contests"
 driver.get(link)
 
+path = os.getcwd()
+
+for a in range(number_int):
+    contest = driver.find_elements_by_link_text("Enter »")[:number_int]
+    contest[a].click()
+    number = 'Most Recent Contest ' + str(a + 1)
+    path1 = os.path.join(path, number)
+    if not os.path.exists(path1):
+        os.mkdir(path1)
+    os.chdir(path1)
+    submissions = driver.find_elements_by_class_name("act")
+    x = len(submissions)
+
+    for j in range(x):
+        problems_codes = driver.find_elements_by_tag_name("td")
+        l = []
+        text = []
+        i = 0
+        while i < len(problems_codes) - 2:
+            l.append(problems_codes[i + 1])
+            text.append(problems_codes[i + 1].text)
+            i += 4
+        list_ofProblems = l[:x]
+        list_ofProblems[j].click()
+        fp = os.path.join(path1, text[j])
+        if not os.path.exists(fp):
+            os.mkdir(fp)
+
+        img = os.path.join(fp, 'problem.png')
+        driver.save_screenshot(img)
+
+        input = driver.find_elements_by_class_name("input")
+        for i in range(len(input)):
+            inp = input[i].text[11:]
+            name = "input" + str(i + 1) + '.txt'
+            filename = os.path.join(fp, name)
+            with open(filename, 'w') as f:
+                f.write(inp)
+
+        output = driver.find_elements_by_class_name("output")
+        for i in range(len(output)):
+            out = output[i].text[12:]
+            name = "output" + str(i + 1) + '.txt'
+            filename = os.path.join(fp, name)
+            with open(filename, 'w') as f:
+                f.write(out)
+
+        driver.back()
+    driver.back()
+
+driver.close()
